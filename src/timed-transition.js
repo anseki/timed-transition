f231f181b160d1ec73723f07cd5a3ebc9fbe249f
anseki <AnSeki.aff@gmail.com>
2017-12-23 17:39:23 +0900

    Fix: traceLog has no [DEBUG] flag

--- src/timed-transition.js
+++ src/timed-transition.js
@@ -121,7 +121,7 @@ function fixCurrentPosition(props) {
   props.currentPosition = props.isOn ?
     Math.min(props.currentPosition + playingTime, props.duration) :
     Math.max(props.currentPosition - playingTime, 0);
-  traceLog.push(`currentPosition:${roundTime(props.currentPosition)}`);
+  traceLog.push(`currentPosition:${roundTime(props.currentPosition)}`); // [DEBUG/]
   traceLog.push('</fixCurrentPosition>'); // [DEBUG/]
 }
 
